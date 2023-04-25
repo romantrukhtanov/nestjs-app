d360d8d08686a14e160f52bccabb8612b2a896e5
romantrukhtanov <romantrukhtanov@gmail.com>
2023-04-25 18:58:51 +0500

    connected mongo db and added base modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { getMongoConfig } from './configs/mongo.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from './schedule/schedule.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: getMongoConfig,
+			inject: [ConfigService],
+		}),
+		ScheduleModule,
+		RoomsModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
